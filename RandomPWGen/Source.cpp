a52abe674c6909194077a6509ec5133594765507
A0L0XIIV <brnky_95@hotmail.com>
2019-05-08 23:14:30 +0300

    Punctuation characters added
    Punctuation character selection added to the generator.

--- RandomPWGen/Source.cpp
+++ RandomPWGen/Source.cpp
@@ -12,42 +12,72 @@ int main() {
 
 		srand(time(NULL));
 
+		char input = '\0';
+		bool hasPunctuationChar = false;
 		int size = 0;
 		int asciiVal;
 		char asciiChar;
 		int intOrChar;
 		string password;
 
+		//Punctuation character question
+		cout << "Do you want punctuation characters like (,!,# or just letters and numbers? (Y/N)" << endl;
+		cin >> input;
+		if (input == 'Y' || input == 'y') {
+			hasPunctuationChar = true;
+		}
+		else {
+			hasPunctuationChar = false;
+		}
+
+		//Size of the password
 		cout << "Enter the size of a password: " << endl;
 		cin >> size;
 
+		//Generate random PW
 		for (int i = 0; i < size; i++) {
 
-			intOrChar = rand() % 3;
+			//Number or letter
+			if (hasPunctuationChar) {
+				intOrChar = rand() % 4;
+			}
+			else {
+				intOrChar = rand() % 3;
+			}
 
 			switch (intOrChar) {
-
+			//Numbers
 			case 0:
 				asciiVal = rand() % 10 + 48;
 				break;
+			//Upper case letters
 			case 1:
 				asciiVal = rand() % 26 + 65;
 				break;
+			//Lower case letters
 			case 2:
 				asciiVal = rand() % 26 + 97;
 				break;
+			//Punctuation characters
+			case 3:
+				asciiVal = rand() % 15 + 33;
 			default:
 				break;
 			}
 
+			//int to char
 			asciiChar = asciiVal;
+			//Detailed operation print
 			if(infLoop == 2)
 				cout << "int:	" << asciiVal << "	" << "char:	" << asciiChar << endl;
+			//Add new character to PW
 			password += asciiChar;
 		}
 		cout << endl;
+		//Result print
 		cout << "Random genereted password: " << password << endl << endl << endl;
 
+		//Program loop
 		cout << "Do you want to generate another random password" << endl;
 		cout << "Enter your choice (0->Finish, 1->Generate, 2->Generate and show all steps): " << endl;
 		if( !(0 <= infLoop <= 2) ){
@@ -56,6 +86,5 @@ int main() {
 		cin >> infLoop;
 
 	}
-
-	//system("pause");
+	return 0;
 }
